fdcd1d508ae78838de14952cb04a707a13650963
Earl Hans Genoso <earlhansg@gmail.com>
2023-06-15 04:21:54 +0800

    UI-Header initial responsiveness

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,11 +10,11 @@ export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary" elevation={0}>
-        <Toolbar sx={{ marginLeft:'15rem', marginRight:'15rem' }}>
-            <Typography variant="h6" component="a" sx={{ width: '30%', display:'flex', justifyContent: "flex-start"}}>
+        <Toolbar sx={{ marginLeft: {lg:'15rem', md:'3rem', xs:'0'}, marginRight:{lg:'15rem', md:'3rem', xs:'0'} }}>
+            <Typography variant="h6" component="a" sx={{ width: {md:'30%', xs:'90%'}, display:'flex', justifyContent: "flex-start"}}>
                 Otaku
             </Typography>
-            <Box sx={{ display:'flex', width:'28%', justifyContent:'space-between', marginRight: 'auto' }}>
+            <Box sx={{ display:{md:'flex', xs:'none'}, width:'28%', justifyContent:'space-between', marginRight: 'auto' }}>
                 {
                     pages.map((page) => (
                         <Typography key={page} component="a" sx={{ fontSize:'16px', fontWeight: 500, lineHeight: '2.1' }}>{page}</Typography>

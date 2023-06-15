e5ffab482996252566b7a9b904358ec1fbaf4733
Earl Hans Genoso <earlhansg@gmail.com>
2023-06-16 03:49:24 +0800

    UI-ItemList finalized with responsiveness

--- frontend/src/components/ItemLIst.tsx
+++ frontend/src/components/ItemLIst.tsx
@@ -1,35 +1,91 @@
+
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
-import zorro from '../images/zorro.png'
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
+import zorro from "../images/zorro.png";
+import Fab from "@mui/material/Fab";
+import Fade from "@mui/material/Fade";
+
+type Item = {
+  id: number;
+  focus: boolean;
+};
 
 export default function ItemLIst() {
+  const [isHovering, setIsHovering] = useState<Item | null>(null);
+
+  const handleMouseOver = (id: number) => {
+    console.log("handleMouseOver", id);
+    const focus = true;
+    setIsHovering({ id, focus });
+  };
+
+  const handleMouseOut = (id: number) => {
+    console.log("handleMouseOut", id);
+    setIsHovering(null);
+  };
   return (
-    <Box mt="3" sx={{marginLeft: { lg: "15rem", xs:"0" },
-    marginRight: { lg: "15rem", xs:"0"}}}>
+    <Box
+      mt="3"
+      sx={{
+        marginLeft: { lg: "15rem", xs: "0" },
+        marginRight: { lg: "15rem", xs: "0" },
+      }}
+    >
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <Box>
-            <Stack direction="row" spacing={2} sx={{width: '100%'}}>
-                <img src={zorro} alt="image" width="100%" height="100%"/>
+        <Grid item md={3} sm={6} xs={12}>
+          <Box m="auto"
+            sx={{ position: "relative", width: {md:"100%", xs:"80%"} }}
+            onMouseOver={() => handleMouseOver(1)}
+            onMouseOut={() => handleMouseOut(1)}
+          >
+            {isHovering?.id === 1 ? (
+              <Fade in={isHovering.focus}>
+                <Fab
+                  color="primary"
+                  aria-label="add"
+                  sx={{ position: "absolute", right: "0", top: "-20px" }}
+                >
+                  View
+                </Fab>
+              </Fade>
+            ) : null}
+            <Stack direction="row" spacing={2} sx={{ width: "100%" }}>
+              <img src={zorro} alt="image" width="100%" height="100%" />
             </Stack>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                First Collection
+            <Typography
+              mt={1}
+              sx={{ fontSize: 19, fontWeight: 500 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              First Collection
             </Typography>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                (209 Previews)
-            </Typography>
-            <Stack sx={{alignItems:'center'}}>
-                <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly sx={{color:"#43616b"}} />
+            <Stack sx={{ alignItems: "center" }}>
+              <Rating
+                name="half-rating-read"
+                defaultValue={2.5}
+                precision={0.5}
+                readOnly
+                sx={{ color: "#43616b" }}
+              />
             </Stack>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                $645.00
+            <Typography
+              sx={{ fontSize: 12 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              (209 Previews)
+            </Typography>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              $645.00
             </Typography>
           </Box>
         </Grid>
